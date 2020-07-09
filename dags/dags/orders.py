e9ec69d0ef5645a81489571f2072943cc8afd20c
Dmitry Logvinenko <d.logvinenko@vezet.ru>
2020-07-09 12:12:28 +0300

    Move start_date to dag

--- dags/dags/orders.py
+++ dags/dags/orders.py
@@ -7,12 +7,9 @@ from airflow.operators.python_operator import PythonOperator
 
 from commons.datasources import sql_server_ds
 
-default_args = {
-    'start_date': datetime(2020, 7, 8, 0)
-    }
 
 dag = DAG('orders', schedule_interval=timedelta(hours=6),
-          default_args=default_args)
+          start_date=datetime(2020, 7, 8, 0))
 
 
 def workflow(**context):
