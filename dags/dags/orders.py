1f92961f7567ddc745a8b83f7b897eb0e56e0572
Dmitry Logvinenko <d.logvinenko@vezet.ru>
2020-07-09 14:39:32 +0300

    Simplify an example dag

--- dags/dags/orders.py
+++ dags/dags/orders.py
@@ -1,13 +1,13 @@
 from datetime import timedelta, datetime
 
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 from commons.datasources import sql_server_ds
 
 
-dag = DAG('orders', schedule_interval=timedelta(hours=6),
+dag = DAG('orders',
+          schedule_interval=timedelta(hours=6),
           start_date=datetime(2020, 7, 8, 0))
 
 
@@ -15,16 +15,9 @@ def workflow(**context):
     print(context)
 
 
-groups = {}
-
 for conn_id, schema in sql_server_ds:
-    group = groups.setdefault(conn_id, DummyOperator(
-        task_id=conn_id, dag=dag))
-
-    load = PythonOperator(
+    PythonOperator(
         task_id=schema,
         python_callable=workflow,
         provide_context=True,
         dag=dag)
-
-    group >> load
