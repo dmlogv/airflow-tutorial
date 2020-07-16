166be941ab9db5648dcb8abb97f01739e3339dd4
Dmitry Logvinenko <d.logvinenko@vezet.ru>
2020-07-16 11:18:46 +0300

    Implement the Session()

--- dags/commons/session.py
+++ dags/commons/session.py
@@ -4,17 +4,89 @@ class Session:
     Example:
         with Session(task_name) as session:
             print(session.id)
+            session.successful = true
             session.loaded_rows = 15
-            session.last_key = 884182
-            session.success = true
             session.comment = 'Well done'
-
     """
-    def __init__(self):
-        pass
+
+    def __init__(self, connection, task_name):
+        self.connection = connection
+
+        self._task_name = task_name
+        self._id = None
+
+        self.loaded_rows = None
+        self.successful = None
+        self.comment = None
 
     def __enter__(self):
-        pass
+        return self.open()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        if any(exc_type, exc_val, exc_tb):
+            self.successful = False
+            self.comment = f'{exc_type}: {exc_val}\n{exc_tb}'
+        self.close()
+
+    @property
+    def task_name(self):
+        return self._task_name
+
+    @property
+    def id(self):
+        return self._id
+
+    def _execute(self, query, *args):
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, args)
+            return cursor.fetchone()[0]
+
+    def _create(self):
+        query = """
+            CREATE TABLE IF NOT EXISTS sessions (
+                id          SERIAL       NOT NULL PRIMARY KEY,
+                task_name   VARCHAR(200) NOT NULL,
+            
+                started     TIMESTAMPTZ  NOT NULL DEFAULT current_timestamp,
+                finished    TIMESTAMPTZ           DEFAULT current_timestamp,
+                successful  BOOL,
+            
+                loaded_rows INT,
+                comment     VARCHAR(500)
+            );
+            """
+        self._execute(query)
+
+    def open(self):
+        query = """
+            INSERT INTO sessions (task_name, finished)
+            VALUES (%s, NULL)
+            RETURNING id;
+            """
+        self.id = self._execute(query, self.task_name)
+        return self
+
+    def close(self):
+        if not self._id:
+            raise SessionClosedError('Session is not open')
+        query = """
+            UPDATE sessions
+            SET
+                finished    = DEFAULT,
+                successful  = %s,
+                loaded_rows = %s,
+                comment     = %s
+            WHERE
+                id = %s
+            RETURNING id;
+            """
+        self._execute(query, self.successful, self.loaded_rows,
+                      self.comment, self.id)
+
+
+class SessionError(Exception):
+    pass
+
+
+class SessionClosedError(SessionError):
+    pass
